8f90321ed6cfe92b4a6afcc5b7993e2d84fe1852
兰长路 <lanchanglu@163.com>
2020-08-23 11:16:43 +0800

    添加路由过渡动画

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app" class="app">
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -10,11 +12,23 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 .app {
   margin: 20px;
   min-width: 720px;
   font-size: 12px;
   color: #333;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
+}
+.fade-enter {
+  opacity: 0;
+  transform: translate(-30px, 0);
+}
+.fade-leave-to {
+  opacity: 0;
+  transform: translate(30px, 0);
 }
 </style>
